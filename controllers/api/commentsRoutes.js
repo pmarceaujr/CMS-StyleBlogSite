ba909d0d94078078b3410c10987d3bcb3c343cb9
Paul Marceau <paul@themarceaus.net>
2021-06-29 00:38:02 -0400

    Added code to allow for adding comments

--- controllers/api/commentsRoutes.js
+++ controllers/api/commentsRoutes.js
@@ -1,8 +1,25 @@
 const router = require('express').Router();
 const withAuth = require('../../utils/auth');
-const { Comments, Review } = require('../../models');
+const { Comments } = require('../../models');
 
 console.log("In company Routes")
 
+//Add a new comment to the selected post 
+router.post('/newComment', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comments.create(
+            {
+                post_id: req.session.post_id,
+                comment_text: req.body.comment,
+                user_id: req.session.user_id,
+            }
+        )
+        res.redirect('/api/posts/posts/' + req.session.post_id);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    };
+});
 
 module.exports = router;
