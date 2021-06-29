ea59c70767edf6156be19786998e2c5b6fc1186a
Paul Marceau <paul@themarceaus.net>
2021-06-29 00:37:44 -0400

    BAse route updates to fix a bug

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,10 +2,23 @@ const router = require('express').Router();
 //const passport = require('passport');
 const { Comments, Users, Posts } = require('../models');
 
-//Get All posts for the home page listing
+console.log("In home Routes")
+//Get All posts for the home page listing order by date created desc (Newest at the top)
 router.get('/', async (req, res) => {
+    console.log("Here I am")
+    console.log(`before: userId: ${req.session.user_id}, loggedIN: ${req.session.loggedIn}`)
     const postsData = await Posts.findAll({
+        order: [['created_at', 'DESC',]],
         include: [
+            {
+                model: Comments,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'user_id',
+                    'post_id',
+                ],
+            },
             {
                 model: Users,
                 attributes: [
@@ -16,15 +29,12 @@ router.get('/', async (req, res) => {
         ],
     });
     // Serialize data so the template can read it
-    const posts = postsData.map((post) => post.get({ plain: true }));
-    console.log("WTF:" + req.session.loggedIn)
+    const posts = await postsData.map((post) => post.get({ plain: true }));
+    console.log(`userId: ${req.session.user_id}, loggedIN: ${req.session.loggedIn}`)
     res.render('homepage', { posts, userId: req.session.user_id, userFirstName: req.session.userFirstName, loggedIN: req.session.loggedIn })
-
 })
 
-console.log("Home routes")
-//Code for the GET routes follows:
-//Get the signup page loaded
+//Get the signup page loaded, if logged in, then display the homepage, otherwise, reload the signup page
 router.get('/signup', async (req, res) => {
     try {
         if (req.session.loggedIn) {
@@ -38,7 +48,7 @@ router.get('/signup', async (req, res) => {
     }
 });
 
-//Get the login page loaded
+//Get the login page loaded, if logged in, then display the homepage, otherwise, reload the login page
 router.get('/login', async (req, res) => {
     try {
         if (req.session.loggedIn) {
@@ -51,7 +61,7 @@ router.get('/login', async (req, res) => {
     }
 });
 
-//Get the logout page loaded
+//Get the logout page loaded and destroy the session
 router.get('/logout', async (req, res) => {
     try {
         if (req.session.loggedIn) {
@@ -63,6 +73,7 @@ router.get('/logout', async (req, res) => {
     }
 });
 
+//The route to pass the HTML category to the api/post route, this will display only HTML posts in the home page
 router.get('/HTML', async (req, res) => {
     try {
         if (req.session.loggedIn) {
@@ -76,6 +87,7 @@ router.get('/HTML', async (req, res) => {
     }
 });
 
+//The route to pass the CSS category to the api/post route, this will display only CSS posts in the home page
 router.get('/CSS', async (req, res) => {
     try {
         if (req.session.loggedIn) {
@@ -89,6 +101,7 @@ router.get('/CSS', async (req, res) => {
     }
 });
 
+//The route to pass the NODE category to the api/post route, this will display only NODE posts in the home page
 router.get('/NODE', async (req, res) => {
     try {
         if (req.session.loggedIn) {
@@ -102,6 +115,7 @@ router.get('/NODE', async (req, res) => {
     }
 });
 
+//The route to pass the REACT category to the api/post route, this will display only REACT posts in the home page
 router.get('/REACT', async (req, res) => {
     try {
         if (req.session.loggedIn) {
@@ -114,4 +128,31 @@ router.get('/REACT', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+//The route to load the add new post form, which allows the user to enter new post data
+router.get('/newPost', async (req, res) => {
+    try {
+        if (req.session.loggedIn) {
+            res.render('newPost', { loggedIN: req.session.loggedIn });
+            return;
+        }
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//The route to load the add new comment form, which allows the user to enter new comment data
+router.get('/newComment', async (req, res) => {
+    try {
+        if (req.session.loggedIn) {
+            res.render('newComment', { loggedIN: req.session.loggedIn });
+            return;
+        }
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
+console.log("In home Index ")
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
-console.log("In Index Routes")
 //Router shortcuts
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-console.log("Exit Index Routes")
+
 module.exports = router;
